44fb2ef1ec441adfab04731fbc3a17c8d9c0ee20
tiepcodegym <viettiep122@gmail.com>
2022-02-09 21:49:03 +0700

    hehe

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+
     public function index()
     {
         $users = User::all();
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,8 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
+Route::prefix( 'auth')->group(function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::middleware('auth:api')->group(function () {
+        Route::post('logout', [AuthController::class,'logout']);
+//    Route::post('refresh', 'AuthController@refresh');
+        Route::post('me', [AuthController::class,'me']);
+    });
 });
 Route::prefix("users")->group(function (){
     Route::get("/",[UserController::class,"index"]);
